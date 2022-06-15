32b2278c28e52f49f02710208eac5128ecc78f59
FindlayJosh <joshuafindlay@gmail.com>
2022-06-15 14:15:35 -0400

    plz

--- server.js
+++ server.js
@@ -71,5 +71,4 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
-console.log(`localhost:${port}`)
+console.log(`http://localhost:${port}`)
